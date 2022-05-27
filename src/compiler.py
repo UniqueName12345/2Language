b45b88526d12f013c69892f27e56a425c455fcec
AAAAAAAAAA <88826851+UniqueName12345@users.noreply.github.com>
2022-05-27 17:37:56 +0800

    Update compiler.py

--- src/compiler.py
+++ src/compiler.py
@@ -26,4 +26,25 @@ def compiler(file_path):
         if "public prog" not in contents:
             print("Error: boilerplate not present in file!")
             return
-        
+    # if boilerplate is present, move onto the next step
+    # check each line of the file
+    with open(file_path, "r") as f:
+        # if the line is a comment, skip it
+        if "&&" in f.readline():
+            return
+        # else if the line is a public prog, skip it
+        elif "public prog" in f.readline():
+            return
+        # else if the line is "terminal.print", print the stuff inside the brackets
+        elif "terminal.print" in f.readline():
+            print(f.readline()[16:-2])
+        # else if the line is "readInt()", ask the user for an integer
+        # example:
+        # readInt("Enter an integer: ")
+        # Output:
+        # 2LANGUAGE: Enter an integer: 5
+        elif "readInt" in f.readline():
+            # input() returns a string, so we need to convert it to an int
+            print(f.readline()[11:-2])
+            print(int(input()))
+        # NOTE: The code is starting to get messy, but it works!
